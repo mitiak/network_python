c5a61153b9c6765637e86d4f15cd342e5e1fe69d
mitiak <mitiak@gmail.com>
2018-08-04 11:42:57 +0300

    echo and multiconn servers and clients completed

---
+++ multiconn_server.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+'''
+Multiconnection server implementation from 
+https://realpython.com/python-sockets/
+'''
+import socket
+import selectors
+import types
+import os
+
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+
+sel = selectors.DefaultSelector()
+
+def accept_wrapper(sock):
+    conn, addr = sock.accept()  # Should be ready to read
+    print('accepted connection from', addr)
+    conn.setblocking(False) # Calls made to this socket will no longer block
+    data = types.SimpleNamespace(addr=addr, inb=b'', outb=b'')
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)  # Should be ready to read
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print('closing connection to', data.addr)
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print('echoing', repr(data.outb), 'to', data.addr)
+            sent = sock.send(data.outb)  # Should be ready to write
+            data.outb = data.outb[sent:]
+
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((HOST, PORT))
+lsock.listen()
+print('listening on', (HOST, PORT))
+
+# Calls made to this socket will no longer block
+lsock.setblocking(False)
+
+# registers the socket to be monitored with sel.select() 
+# for the events you’re interested in. For the listening
+# socket, we want read events: selectors.EVENT_READ
+sel.register(lsock, selectors.EVENT_READ, data=None)
+
+
+while True:
+    events = sel.select(timeout=None)
+    for key, mask in events:
+        if key.data is None:
+            accept_wrapper(key.fileobj)
+        else:
+            service_connection(key, mask)
+
